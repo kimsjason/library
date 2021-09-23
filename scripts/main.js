60569851ae815f542faed21ab397e4c83fda9aed
Jason Kim <jason.s.kim95@gmail.com>
2021-09-23 12:40:54 -0400

    Add Book constructor

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readBook = readBook;
+    info = function () {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readBook}`
+    }
+}
