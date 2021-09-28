4df77134a056c4e415e0033d9f46b266d15576c4
Jason Kim <jason.s.kim95@gmail.com>
2021-09-28 00:12:04 -0400

    Add ability to update read status on a book

--- scripts/main.js
+++ scripts/main.js
@@ -19,17 +19,26 @@ function createBookElement(book) {
     card.classList.add('card');
 
     const removeBook = document.createElement('button');
+    const index = myLibrary.indexOf(book);
     removeBook.classList.add('remove-book');
     removeBook.addEventListener('click', () => {
         removeBook.parentElement.remove();
-        const index = myLibrary.indexOf(book);
         myLibrary.splice(index, 1);
-    })
+    });
 
     const title = document.createElement('div');
     const author = document.createElement('div');
     const pages = document.createElement('div');
-    const readBook = document.createElement('div');
+    const readBook = document.createElement('input');
+    readBook.type = 'checkbox';
+    if (book.readBook == null) {
+        readBook.checked = false;
+    } else {
+        readBook.checked = true;
+    }
+    readBook.addEventListener('click', () => {
+        myLibrary[index].readBook = readBook.checked;
+    });
 
     title.textContent = book.title;
     author.textContent = book.author;
@@ -65,8 +74,8 @@ newBook.addEventListener('click', () => {
         const title = document.querySelector('input[name=title]');
         const author = document.querySelector('input[name=author]');
         const pages = document.querySelector('input[name=pages]');
-        const readBook = document.querySelector('input[name="read book"]:checked');
-        const book = new Book(title.value, author.value, pages.value, readBook.value);
+        const readBook = document.querySelector('input[name="read book"]');
+        const book = new Book(title.value, author.value, pages.value, readBook.checked);
 
         addBookToLibrary(book);
         document.body.appendChild(createBookElement(book));
